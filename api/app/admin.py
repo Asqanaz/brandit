364e069776515cf5ab375be2b6ed5601cfccc84a
avagyansamvel <avagyansamvel07@gmail.com>
2022-08-07 17:48:08 +0400

    some small changes

--- api/app/admin.py
+++ api/app/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from .models import *
 
+admin.site.site_header = 'Brandit Administration'
+admin.site.site_title = 'Brandit Administration'
+
 admin.site.register(Portfolio)
 admin.site.register(ProjectType)
 admin.site.register(ProjectDesign)
