ac245fb24a69d13f7fca3c6ae1ef6096943dce60
avagyansamvel <avagyansamvel07@gmail.com>
2022-09-11 16:11:07 +0400

    All changed

--- api/api/settings.py
+++ api/api/settings.py
@@ -128,7 +128,9 @@ EMAIL_PORT = 587
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
